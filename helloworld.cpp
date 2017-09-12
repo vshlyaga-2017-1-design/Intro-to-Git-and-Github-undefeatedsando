3906eefef2dfade0e0aac805480e5c658c104807
undefeatedsando <thekaidaten@gmail.com>
2017-09-12 23:51:52 +0300

    Комментарии действительно важны closes #1

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
+	//Hash is 1f1a7d6
 	time_t t = time(0);   // get time now
     struct tm * now = localtime( & t );
 	cout << "Hello, User! Today is" << now->tm_mday << '.' << now->tm_mon+1 << '.' << now->tm_year+1900;
